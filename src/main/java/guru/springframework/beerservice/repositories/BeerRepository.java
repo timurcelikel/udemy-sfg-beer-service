c794c778f4b180d6eb5a58c0888f3a8497e5a612
Timur Celikel <timur.celikel@proton.me>
2023-12-22 10:26:10 -0800

    Removed unnecessary method declaration in interface

--- src/main/java/guru/springframework/beerservice/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/beerservice/repositories/BeerRepository.java
@@ -3,11 +3,11 @@ package guru.springframework.beerservice.repositories;
 import guru.springframework.beerservice.domain.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
 
-	Optional<Beer> findById(UUID beerId);
+	// Below is not needed as it overrides one of the methods we get for free from JpaRepository
+	//Optional<Beer> findById(UUID beerId);
 
 }
